f6d7dd24e03bd78388ae0a4fb9f5d08328add310
NilsLeVrai <nilslucas@wanadoo.fr>
2026-06-22 18:53:36 +0200

    feature-BLKnniversaire: removed discord-example folder

--- BLKnniversaire/main.py
+++ BLKnniversaire/main.py
@@ -1,10 +1,12 @@
 import asyncio
 import os
 from dotenv import load_dotenv
+from bot import init
 
 load_dotenv()
 
 async def start():
+    init()
     token = os.getenv("DISCORD_TOKEN")
     id__ = os.getenv("APP_ID")
     print(f"DISCORD_TOKEN : {token}")
